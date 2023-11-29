af98aa092fc99c9e3ff8b1ba4af3cd1a22e90361
k-suke39 <pz.mozarla@gmail.com>
2023-11-29 14:44:55 +0900

    [add]

--- 03_js_basic/090_promise/end/main.js
+++ 03_js_basic/090_promise/end/main.js
@@ -2,16 +2,25 @@
 let a = 0;
 
 new Promise((resolve, reject) => {
-    setTimeout(() => {
-        a = 1;
-        resolve(a)
-    }, 2000);
-}).then((b) => {
-    console.log(b);
-    return b;
-}).then((b) => {
-    console.log(b);
-}).catch((c) => {
-    console.log('catchが実行', c)
+	setTimeout(() => {
+		a = 1;
+		resolve(a);
+	}, 2000);
 })
+	.then((b) => {
+		console.log(b);
+		return b;
+	})
+	.then((b) => {
+		console.log(b);
+	})
+	.catch((c) => {
+		console.log('catchが実行', c);
+	});
 
+//Promoiseは非同期オブジェクト
+//非同期の時点でresolveもしくはrejectプロパティを持った
+//resolveを使うと、thenが実行される
+//rejectが実行されると、
+//メソッドチェーン見づらいよね
+//async/await使おうぜ
