58cc8d8ba6e913b8aab2550066a089e8147d7104
k-suke39 <pz.mozarla@gmail.com>
2023-11-29 15:13:39 +0900

    [add]

--- 03_js_basic/090_promise/end/main.js
+++ 03_js_basic/090_promise/end/main.js
@@ -1,26 +1,24 @@
-// POINT 非同期処理（Promise）
-let a = 0;
-
-new Promise((resolve, reject) => {
-	setTimeout(() => {
-		a = 1;
-		resolve(a);
-	}, 2000);
-})
-	.then((b) => {
-		console.log(b);
-		return b;
-	})
-	.then((b) => {
-		console.log(b);
-	})
-	.catch((c) => {
-		console.log('catchが実行', c);
-	});
-
 //Promoiseは非同期オブジェクト
 //非同期の時点でresolveもしくはrejectプロパティを持った
 //resolveを使うと、thenが実行される
 //rejectが実行されると、
 //メソッドチェーン見づらいよね
 //async/await使おうぜ
+
+//非同期におけるcallback問題
+//①callback地獄
+//try/catchの中に書いてもエラーになる、catchするには
+//非同期の中でtrycatchする必要がある
+
+//第一引数にcallbackを指定する必要がある
+//PromiseStateがpending
+//PromiseResultがundefined
+//let promise = new Promise(() => {});
+//console.log(promise);
+
+//PromiseStateがfullfileed
+//PromiseResultがhello
+let promise = new Promise((resolve) => {
+	resolve('hello');
+});
+console.log(promise);
