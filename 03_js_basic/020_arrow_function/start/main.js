e752ccfdc47408d5149573640dc44165e0bf4b2e
k-suke39 <pz.mozarla@gmail.com>
2023-11-29 11:32:44 +0900

    [add]

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,17 @@
+//関数宣言
 function fn(number) {
-  return number * 2;
+	return number * 2;
 }
+
+//関数式
+const fn = function (number) {
+	return number * 2;
+};
+
+//関数式のアロー関数
+const fn = (number) => {
+	return number * 2;
+};
+
+//関数式のアロー関数の省略記法
+const fn = (number) => number * 2;
